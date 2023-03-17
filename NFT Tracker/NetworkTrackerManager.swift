53fcb58190cd71c9d66da9fa1ac88c2a5c9599d7
Stepan <ifomstep@icloud.com>
2023-03-17 23:13:09 +0500

    Added Network Manager

---
+++ NFT Tracker/NetworkTrackerManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkTrackerManager.swift
+//  NFT Tracker
+//
+//  Created by Степан Фоминцев on 17.03.2023.
+//
+
+import Foundation
+
+struct NetworkTrackerManager {
+    func fetchFromWallet() {
+        
+    }
+}
